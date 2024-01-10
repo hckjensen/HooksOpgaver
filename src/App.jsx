f569b8db799f630962b328ca18db3fceae61309c
hckjensen <hckjensen96@gmail.com>
2024-01-10 13:08:32 +0100

    ywy

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Counter from './components/useEffectTimer'
+import './App.scss'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 style={{
+        textAlign: 'center',
+        color: 'black',
+        fontSize: '4rem'
+      }}>
+        React Hooks
+      </h1 >
+      <Counter />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/useEffectTimer.jsx
@@ -0,0 +1,94 @@
+import { useState, useEffect } from 'react';
+
+
+
+
+const Counter = () => {
+
+    useEffect(() => {
+        document.title = 'React Hooks';
+    }, []);
+
+    const [count, setCount] = useState(0);
+    const [isActive, setIsActive] = useState(false);
+    const [intervalId, setIntervalId] = useState(null);
+
+    useEffect(() => {
+        if (isActive) {
+            const id = setInterval(() => {
+                setCount(count => count + 1);
+            }, 1000);
+            setIntervalId(id);
+        } else if (!isActive && intervalId) {
+            clearInterval(intervalId);
+        }
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [isActive]);
+
+    const handleStart = () => {
+        setIsActive(true);
+    };
+
+    const handleStop = () => {
+        setIsActive(false);
+    };
+
+    const handleReset = () => {
+        setCount(0);
+    }
+
+
+    return (
+
+        <>
+            <h1 style={{
+                textAlign: 'center',
+                color: 'black',
+                fontSize: '2rem'
+
+            }}>Stopwatch</h1>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                marginTop: '6rem',
+
+            }}>
+                <p style={{
+                    display: 'flex',
+                    width: '10rem',
+                    height: '5rem',
+                    fontSize: '2rem',
+                    color: 'lightgreen',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    backgroundColor: 'black',
+                    borderRadius: '1rem',
+
+                }}> {count}</p>
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    gap: '1rem',
+                    marginTop: '6rem'
+                }}>
+                    <button onClick={handleStart}>Start</button>
+                    <button onClick={handleStop}>Stop</button>
+                    <button onClick={handleReset}>Reset</button>
+                </div>
+            </div >
+
+
+        </>
+    )
+
+}
+
+
+export default Counter;
